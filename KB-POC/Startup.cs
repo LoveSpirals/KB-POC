0bffc27395db454a4df1ee5f66a9ce1d41f87f1d
LoveSpirals <pskovacs@hotmail.com>
2018-03-20 18:14:36 -0700

    Attempt at role-based credentials

--- KB-POC/Startup.cs
+++ KB-POC/Startup.cs
@@ -7,6 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Configuration;
+
 namespace POC_1
 {
     public class Startup
@@ -14,7 +18,11 @@ namespace POC_1
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-        }
+            string clientId = configuration["ida:ClientId"];
+            string aadInstance = configuration["ida:AADInstance"];
+            string tenant = configuration["ida:Tenant"];
+            string postLogoutRedirectUri = configuration["ida:PostLogoutRedirectUri"];
+    }
 
         public IConfiguration Configuration { get; }
 
@@ -22,6 +30,14 @@ namespace POC_1
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddAuthentication()
+                .AddCookie()
+                .AddMicrosoftAccount(opts =>
+                {
+                    opts.ClientId = "bf79dc67-aa09-4171-8dd2-5a3f60e16586";
+                    opts.ClientSecret = "IxOlVVN3ljWb6dZZ8/iwSen+YF4Fuvqygndv0JnDYsY=";
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +54,8 @@ namespace POC_1
             }
 
             app.UseStaticFiles();
+
+            app.UseAuthentication();
 
             app.UseMvc();
         }
